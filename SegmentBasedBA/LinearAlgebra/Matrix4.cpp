b089c53a4d78bd8530a197cb317f0fc6a8d76a4f
ZJUCVG <ZJUCVG>
2018-06-05 16:13:31 +0800

    add copyright to source

--- SegmentBasedBA/LinearAlgebra/Matrix4.cpp
+++ SegmentBasedBA/LinearAlgebra/Matrix4.cpp
@@ -1,3 +1,24 @@
+/**
+* This file is part of SegmentBA.
+*
+* Copyright (C) 2017 Zhejiang University
+* For more information see <https://github.com/ZJUCVG/SegmentBA>
+* If you use this code, please cite the corresponding publications as 
+* listed on the above website.
+*
+* SegmentBA is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SegmentBA is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SegmentBA. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "stdafx.h"
 #include "Matrix4.h"
